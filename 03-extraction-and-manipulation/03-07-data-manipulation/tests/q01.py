6958281932bc3720b70e785f6755e3a66c1b930d
Jitendra Singh <singhj@datathinks.org>
2020-11-05 17:54:58 -0500

    notebook 03-07

---
+++ 03-extraction-and-manipulation/03-07-data-manipulation/tests/q01.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> nf
+          array([[ 20,   2],
+                 [100,   4],
+                 [ 30,   2]]
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
