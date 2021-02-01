a9f3830c64ea39dc2d9a37768d6a6ed8ed726c3f
singhj <singhj@datathinks.org>
2021-02-01 06:19:00 -0500

    rally_clicks_loader

--- rally/aws_s3.py
+++ rally/aws_s3.py
@@ -1,5 +1,4 @@
 import boto3
-_secrets_ = ('AKIA5R2GKQRMQ7ROQVXZ', '3KD0qDhdtpcPna5ETnkg7DKtY5TneAxoV3Iskd81')
 session = boto3.session.Session(aws_access_key_id=_secrets_[0],
                                 aws_secret_access_key=_secrets_[1],
                                 aws_session_token=None,
